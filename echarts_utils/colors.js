c809d76400ef1f1d9953ef8517e2909d69f76b9a
Pawel Lulinski <pawel.lulinski@laboratorium.ee>
2018-09-20 09:54:17 +0200

    BarChart component added and echarts colors

---
+++ echarts_utils/colors.js
@@ -0,0 +1,8 @@
+export const BLACK = "#000000";
+export const GRAY = "#757575";
+export const ORANGE = "#D68963";
+export const PURPLE = "#9C27B0";
+export const RED = "#FF0062";
+export const VIOLET = "#C2C2E2";
+export const TURQUOISE = "#00BCD4";
+export const WHITE = "#FFFFFF";
---
+++ echarts_utils/configuration/defaultToolboxProperties.js
@@ -0,0 +1,9 @@
+export default {
+    show: true,
+    orient: "vertical",
+    left: "right",
+    top: "middle",
+    itemSize: 25,
+    itemGap: 35,
+    padding: [0, 30, 0, 0],
+};
