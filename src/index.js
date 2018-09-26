00e47aae735d4f677e14bb71e95b9df7e9e0be44
plulee <32864746+plulee@users.noreply.github.com>
2018-09-26 12:16:02 +0200

    fixes in styles, and Header added to components (#9)

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import { histogramForBarChart } from "./histogram";
 import * as colors from "./echarts_utils/colors";
 import createNumberFormatter from "./echarts_utils/tools/formatters";
 import BarChart from "./echarts_utils/components/BarChart";
+import Header from "./echarts_utils/components/Header";
 
-export default { histogramForBarChart, BarChart, createNumberFormatter, colors };
+export default { histogramForBarChart, BarChart, Header, createNumberFormatter, colors };
