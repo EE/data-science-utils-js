e2b653d8a89addee31816c76f97cd603082ed74e
Pawel Lulinski <pawel.lulinski@laboratorium.ee>
2018-09-28 12:54:38 +0200

    unnecesary file removed

--- src/echarts_utils/components/index.js
+++
@@ -1,7 +0,0 @@
-export {default as Article} from "./Article";
-export {default as BarChart} from "./BarChart";
-export {default as Header} from "./Header";
-export {default as MapChart} from "./MapChart";
-export {default as NumberWidget} from "./NumberWidget";
-export {default as Paragraph} from "./Paragraph";
-export {default as ScatterChart} from "./ScatterChart";
